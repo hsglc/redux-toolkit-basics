afb21f811987727547d9a882f929e03a2182cfd5
huseyin gulcu <huseyingulcuu@gmail.com>
2021-05-10 16:26:57 +0300

    cart component's relationship to other components has handled.

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,25 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  { id: "p1", price: 6, title: "A Book", description: "The first book" },
+  { id: "p2", price: 5, title: "Second Book", description: "The second book" },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((item) => (
+          <ProductItem
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+            id={item.id}
+          />
+        ))}
       </ul>
     </section>
   );
